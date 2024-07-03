091bed3904870b61718c3551d0ad1e6f3cd14025
raitocc <raitocc@qq.com>
2024-07-03 11:17:50 +0800

    LoginWidget添加

--- Tank_Trouble_IV/mainwindow.h
+++ Tank_Trouble_IV/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "loginwidget.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +20,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    LoginWidget* mLoginWidget;
 };
 #endif // MAINWINDOW_H
