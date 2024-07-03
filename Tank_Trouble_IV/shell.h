a909c33c088ff21bad2489033ae20d8d94540554
333 <14630384+tanyeshan@user.noreply.gitee.com>
2024-07-03 17:18:10 +0800

    完善子弹属性

--- Tank_Trouble_IV/shell.h
+++ Tank_Trouble_IV/shell.h
@@ -1,18 +1,78 @@
-#ifndef SHELL_H
 #define SHELL_H
-#include <Qstring>
-using namespace std;
+
+#include<QMouseEvent>
 #define basic_shell_speed 20;
+
+enum shell_name={"普通子弹","爆炸弹","穿透弹","穿墙跟踪弹"}
+
 class shell
 {
-    string name;//炮弹名称
+public:
+    //构造函数
+    //子弹目前尚不存在
+    shell(shell_name sn,int ID,bool ifexist = 0) :name(sn),ID(ID),status(ifexist)
+    {
+        switch(sn){
+        case "普通子弹":
+            ATK=2;
+            width=1;
+            shell_speed=0.8*basic_shell_speed;
+            break;
+        case "爆炸弹":
+            ATK=4;
+            width=3;
+            shell_speed=0.5*basic_shell_speed;
+            break;
+        case "穿透弹":
+            ATK=4;
+            width=1;
+            shell_speed=1.2*basic_shell_speed;
+            break;
+        case "穿墙跟踪弹":
+            ATK=2;
+            width=1;
+            shell_speed=0.8*basic_shell_speed;
+            break;
+        default:
+            break;
+        }
+    }
+    ~shell(){};
+    void updatePosition();
+    void mousePressEvent(QMouseEvent *event);
+    bool shouldRemove();
+
+    shell_name name;//炮弹名称
     int ID;//编号便于记录
     int ATK;//攻击力
     int shell_speed;//子弹速度
     int width;//子弹宽度，间接定义碰撞体积
     double shell_x;
-    double shell_Y;//炮弹的坐标
-
+    double shell_y;//炮弹的坐标
+    bool status;//是否存在的状态
 };
 
-#endif // SHELL_H
+// 更新子弹位置
+shell::void updatePosition() {
+    if(status=1)
+    {
+
+    }
+}
+
+// 按下鼠标左键发射子弹
+shell::void shell::mousePressEvent(QMouseEvent *event){
+    if(event->button()==Qt::LeftButton)
+    {
+        status=1;
+    }
+    updatePosition();
+}
+
+// 检查子弹是否应该被移除
+shell::bool shouldRemove() {
+    // 根据实际情况判断子弹是否应该被移除
+    // 例如，子弹是否已经飞出屏幕
+    return !status;
+}
+
