c1ae4463b931d9090da779e0e80ec1de163809e4
yang <2801672379@qq.com>
2024-07-05 17:23:42 +0800

    创建并连接云数据库

--- Tank_Trouble_IV/registerdialog.cpp
+++ Tank_Trouble_IV/registerdialog.cpp
@@ -6,16 +6,55 @@
 #include <QFile>
 #include <QTextStream>
 
+#include <QCoreApplication>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QDebug>
+#include <QSqlError>
+
 RegisterDialog::RegisterDialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::RegisterDialog)
 {
     ui->setupUi(this);
     this->setWindowTitle("Register");
+
+    // 创建并配置数据库连接
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+    db.setHostName("rm-cn-j4g3tbaf00003b.rwlb.rds.aliyuncs.com");
+    db.setPort(3306);
+    db.setDatabaseName("ali");
+    db.setUserName("yrxY");
+    db.setPassword("Yrx200555");
+
+    // 尝试打开数据库连接
+    if (db.open()) {
+        qDebug() << "数据库连接成功";
+
+    } else {
+        qDebug() << "数据库连接失败:" << db.lastError().text();
+    }
+
+    // //创建表
+    // QSqlQuery query;
+    // if (query.exec("CREATE TABLE users ("
+    //                "username VARCHAR(50) NOT NULL,"
+    //                "password VARCHAR(255) NOT NULL,"
+    //                "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+    //                ")")) {
+    //     qDebug() << "表创建成功";
+    // } else {
+    //     qDebug() << "创建表失败:" << query.lastError().text();
+    // }
+
+    // QSqlQuery query;
+    // query.exec("insert into users(username,password) values('mike',1)");
 }
 
 RegisterDialog::~RegisterDialog()
 {
+    // 关闭数据库连接
+    QSqlDatabase::database().close();
     delete ui;
 }
 
@@ -59,20 +98,18 @@ bool RegisterDialog::isInputValid()//输入是否合法
 
 bool RegisterDialog::isUserRegistered(const QString &username)//用户名是否已经被注册过
 {
-    QFile file("userdata.txt");
-    if(file.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-        QTextStream in(&file);
-        while(!in.atEnd())
-        {
-            QString line = in.readLine();
-            QStringList parts = line.split(",");
-            if(parts.size() == 2 && parts[0] == username)
-            {
+    QSqlQuery query;
+    query.prepare("SELECT COUNT(*) FROM users WHERE username = :username");
+    query.bindValue(":username",username);
+    if(query.exec()){
+        if(query.next()){
+            int count = query.value(0).toInt();
+            if(count>0){
+                //用户名已存在
+                qDebug() << "该用户名已被注册";
                 return true;
             }
         }
-        file.close();
     }
     return false;
 }
@@ -81,34 +118,32 @@ void RegisterDialog::on_registerButton_clicked()//注册按钮按下
 {
     if(isInputValid())
     {
-        QString username = ui->usernameLineEdit->text();
-        QString password = ui->passwordLineEdit->text();
+        QString new_username = ui->usernameLineEdit->text();
+        QString new_password = ui->passwordLineEdit->text();
 
         // 检查用户是否已存在
-        if(isUserRegistered(username))
+        if(isUserRegistered(new_username))
         {
             QMessageBox::warning(this, tr("Registration Error"), tr("Username already registered."));
             return;
         }
 
         // 将新用户信息写入文件
-        QFile file("userdata.txt");
-        if(file.open(QIODevice::Append | QIODevice::Text))
-        {
-            QTextStream out(&file);
-            out << username << "," << password << "\n";
-            file.close();
+        QSqlQuery query;
+        if (query.exec("insert into users (username, password) values ('" + new_username + "', '" + new_password + "')")) {
+            qDebug() << "用户注册成功";
             QMessageBox::information(this, tr("Registration"), tr("Registration Successful!"));
             accept();
         }
-        else
-        {
-            QMessageBox::warning(this, tr("Error"), tr("Could not open file for writing."));
+        else {
+            qDebug() << "用户注册失败:" << query.lastError().text();
         }
+    }
+    else {
+        qDebug() << "无法打开数据库连接";
     }
 
 }
-
 
 void RegisterDialog::on_cancelButton_clicked()
 {
