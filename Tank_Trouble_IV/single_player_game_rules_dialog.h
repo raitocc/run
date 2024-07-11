a5cad6cb2c2e97d52407acf9950c446ee99b322e
333 <14630384+tanyeshan@user.noreply.gitee.com>
2024-07-11 16:35:20 +0800

    美化

---
+++ Tank_Trouble_IV/single_player_game_rules_dialog.h
@@ -0,0 +1,25 @@
+﻿ #ifndef SINGLE_PLAYER_GAME_RULES_DIALOG_H
+#define SINGLE_PLAYER_GAME_RULES_DIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class Single_player_game_rules_Dialog;
+}
+
+class Single_player_game_rules_Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Single_player_game_rules_Dialog(QWidget *parent = nullptr);
+    ~Single_player_game_rules_Dialog();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::Single_player_game_rules_Dialog *ui;
+};
+
+#endif // SINGLE_PLAYER_GAME_RULES_DIALOG_H
